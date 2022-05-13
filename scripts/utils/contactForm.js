9efb5d0e4458602ec229bc1ce13345c85652f257
Cynthia Caron <caron.cynthia@outlook.fr>
2022-05-13 08:05:24 +0200

    modal contact

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,11 +1,15 @@
 function displayModal() {
   const modal = document.getElementById("contact_modal");
   const main = document.getElementById("main");
+  const header = document.getElementById("header");
   modal.style.display = "block";
   main.className = "bg_blur";
+  header.className = "bg_blur";
 }
 
 function closeModal() {
   const modal = document.getElementById("contact_modal");
   modal.style.display = "none";
+  main.className = "blur_none";
+  header.className = "blur_none";
 }
